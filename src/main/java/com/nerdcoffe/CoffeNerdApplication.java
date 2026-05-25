c2490df7626f41354db4faf32123a218f8dd3d94
WilliamSoares21 <pedro.williamsoares2022@gmail.com>
2026-05-25 14:28:09 -0300

    Initial commit

---
+++ src/main/java/com/nerdcoffe/CoffeNerdApplication.java
@@ -0,0 +1,13 @@
+package com.nerdcoffe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeNerdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeNerdApplication.class, args);
+	}
+
+}
